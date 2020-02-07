8c70ec5f99c67bc1336f3098109679f76a2d2ac2
nola360 <adaramolajr@gmail.com>
2020-02-07 00:10:38 -0500

    revealing module pattern

--- app.js
+++ app.js
@@ -19,21 +19,50 @@
 
 // MODULE PATTERN EXAMPLE 1
 
-const UICtrl = (function () {
-  let text = 'Hello World!';
+// const UICtrl = (function () {
+//   let text = 'Hello World!';
 
-  const changeText = function () {
-    const element = document.querySelector('h1');
-    element.textContent = text;
+//   const changeText = function () {
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+//   }
+
+//   return {
+//     callChangeText: function () {
+//       changeText();
+//       console.log(text);
+//     }
+//   }
+// })();
+
+// UICtrl.callChangeText();
+
+// REVEALING MODULE PATTERN
+const ItemCtrl = (function () {
+  let data = [];
+
+
+  function add(item) {
+    data.push(item);
+    console.log('Item added...')
+  }
+
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
   }
 
   return {
-    callChangeText: function () {
-      changeText();
-      console.log(text);
-    }
+    add: add,
+    get: get
   }
+
 })();
 
-UICtrl.callChangeText();
 
+ItemCtrl.add({ id: 1, name: 'James}' })
+// ItemCtrl.add({ id: 1, name: 'Jen}' })
+// ItemCtrl.add({ id: 1, name: 'John}' })
+console.log(ItemCtrl.get(1));
